9c1905df75ac642c0faed37d3bf9f08110dc0508
Alberto Robledo <roblo53@hotmail.com>
2020-05-03 22:19:08 -0300

    arduino code is loaded and the file is edited read

---
+++ P-Keeper-Arduino.ino
@@ -0,0 +1,70 @@
+/* Control velocidad y sentido giro de motor PaP con pololu A4988 
+ *  2020 - P-keeper
+ */
+//https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads/
+#include <Wire.h>			// libreria de comunicacion por I2C
+#include <LCD.h>			// libreria para funciones de LCD
+#include <LiquidCrystal_I2C.h>		// libreria para LCD por I2C
+
+int steps = 9;       // pin step 9
+int direccion = 3;   // pin direccion 3
+int potenciometro;   // lectura del potenciometro
+int potenciometro2; // lectura del potenciometro2
+int boton = 7;       // pin pulsador 7
+int porRetVeloc;
+int porRetInaExa;
+int analogico0;
+int analogico1;
+LiquidCrystal_I2C lcd (0x27, 2, 1, 0, 4, 5, 6, 7); // DIR, E, RW, RS, D4, D5, D6, D7
+
+void setup() {                
+  // inicializamos pin como salidas.
+  pinMode(steps, OUTPUT); 
+  pinMode(direccion, OUTPUT); 
+  // inicializamos pin como entrada.
+  pinMode(boton, INPUT);
+  // Indicar a la libreria que tenemos conectada una pantalla de 16x2
+  lcd.setBacklightPin(3,POSITIVE);	// puerto P3 de PCF8574 como positivo
+  lcd.setBacklight(HIGH);		// habilita iluminacion posterior de LCD
+  lcd.begin(16, 2);			// 16 columnas por 2 lineas para LCD 1602A
+  lcd.clear();			// limpia pantalla
+  lcd.print("thepkeeper"); // Enviar el mensaje
+  lcd.setCursor(0, 1);
+  lcd.print(".wordpress.com");
+  delay(3000);
+  lcd.clear();
+}
+
+void loop() {
+    analogico0 = analogRead(A0);     // leemos el potenciometro en el puerto A0
+    porRetVeloc = map(analogico0, 0, 1024, 100, 0);
+    potenciometro = map(analogico0,0,1024,250,4000);  // adaptamos el valor leido a un retardo 250microSeg y 4000microSeg
+    analogico1 = analogRead(A1);     // leemos el potenciometro en el puero A1
+    porRetInaExa = map(analogico1, 0, 1024, 0, 100);
+    potenciometro2 = map(analogico1,0,1024,300,2500);  // adaptamos el valor leido a un retardo de 0.3 a 2.5seg
+    int sentido = digitalRead(boton);    // leemos el boton de direccion
+    digitalWrite(direccion, sentido);    // cambiamos de dirección segun pulsador
+    //PANTALLA
+    lcd.setCursor(0, 0);		// ubica cursor en columna 0 y linea 0
+    lcd.print(porRetVeloc);  // Valor pauda en microsegundo
+    lcd.print("% Breathing.V");
+    lcd.setCursor(0, 1);
+    lcd.print(porRetInaExa);  // Valor pauda en microsegundo
+    lcd.print("% Inhal.Pause");
+    //INAHALACION
+    for (int x = 0; x < 1370 ; x++) {      // 200 pasos para 180° y como tiene un reductor se necesita 1370 pasos para 180°
+      digitalWrite(steps, HIGH);         // Aqui generamos un flanco de bajada HIGH - LOW
+      delayMicroseconds(5);              // Pequeño retardo para formar el pulso en STEP
+      digitalWrite(steps, LOW);         // y el A4988 de avanzara un paso el motor
+      delayMicroseconds(potenciometro); // generamos un retardo con el valor leido del potenciometro
+    }
+    delay(potenciometro2); // retardo de inahalacion y exalacion
+    //EXALACION
+    for (int x = 0; x < 1370 ; x++) {      // 200 pasos para 180° y como tiene un reductor se necesita 1370 pasos para 180°
+      digitalWrite(steps, HIGH);         // Aqui generamos un flanco de bajada HIGH - LOW
+      delayMicroseconds(5);              // Pequeño retardo para formar el pulso en STEP
+      digitalWrite(steps, LOW);         // y el A4988 de avanzara un paso el motor
+      delayMicroseconds((potenciometro + 800)); // generamos un retardo con el valor leido del potenciometro Y LE sumamos 800MICROSEGUNDOS
+    }
+    delay(potenciometro2); // retardo de inahalacion y exalacion
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # P-Keeper
 Respirador artificial para el COVID-19, Artificial respirator for COVID-19
+![](images/pkeeper1.jpeg 200x200)
+
+# Como Funciona?
+## Todo respirador debe cumplir con los parametros:
+
+- Volumen de ingreso de aire: de 6 a 8ml/kg.
+- Presión de ingreso de aire: determinado por el medico.
+- Frecuencia respiratoria: 10 a 20rpm regulable o programable
+- Pausa entre inhalación y exhalación regulable.
+- Posibilidad de extracción de aire si el paciente no puede exhalar por sus medios.
+
+# Para lograr todos estos parametros P-KEEPER utiliza:
+
+- 2 fuelles de 5lts de volumen máximo que pueden ser regulados hasta los 200ml, los cuales sirven: 1 para el ingreso de aire y otro para extracción del aire en el caso que el paciente no puede hacerlo por sus medios pero puede operar con solo 1 fuelle para la inhalación.
+- 2 motores paso a paso, 1 capaz de aportar movimiento continuo las 24hs por 80 días antes de una revisión en sus rodamientos y el otro como sistema redundante de backup el cual está montado sobre el mismo eje y mide en todo momento el esfuerzo que el motor 1 realiza y cuando se sale de parámetros, se hace una conmutación para apagar un motor y arrancar el otro de manera instantánea y en la misma posición, mostrando con un led que el motor 1 está fuera de servicio .
+- Sistema de control electrónico análogo para regular parámetros con simples potenciómetros, que regulan el tiempo de giro de los motores y frecuencias internas que permiten que el motor realice pausas en algún momento del recorrido.
+- Ruedas de accionamientos.
+- Levas de accionamiento regulables sobre las ruedas para poder regular el volumen de aire.
+- Válvulas tipo diafragma para las mangueras y conexiones standard para cualquier manguera de respirador en el mercado.
+- Cámara mezcladora de aire y oxigeno en el aspirado del fuelle.
+
+# Como funciona?
+
+El motor paso a paso, mueve por unos engranes y poleas la rueda de transmisión que tiene sujetas las levas de accionamiento que mueven los fuelles, uno sube cuando el otro baja y por ende, uno inyecta aire y el otro lo extrae del paciente, con unos potenciómetros se pueden regular las velocidades y pausas pero a su vez con tan solo un ajuste mecánico en la rueda de transmisión se puede graduar el volumen de aire que se quiere inyectar y extraer del paciente. También posee sistema de bateria de 12v que permiten mantener el funcionamiento por al menos 12hs sin conexión a la red eléctrica.
